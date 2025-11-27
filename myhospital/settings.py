06354f1d2bae169b325cd568798e610042d7408a
Nitinpatel2532 <nitinpatel74084@gmail.com>
2025-11-28 03:17:27 +0530

    Configured PostgreSQL

--- myhospital/settings.py
+++ myhospital/settings.py
@@ -92,12 +92,17 @@ WSGI_APPLICATION = "myhospital.wsgi.application"
 # ================================
 # DATABASE
 # ================================
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        config("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # ================================
 # PASSWORD VALIDATORS
