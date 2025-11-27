11802fba32f47525835d77f8cedd94038c9957f7
Nitinpatel2532 <nitinpatel74084@gmail.com>
2025-11-28 02:46:45 +0530

    updated  settings.py

--- myhospital/settings.py
+++ myhospital/settings.py
@@ -135,6 +135,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "https://hopewell-hospital-management-system.vercel.app",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
